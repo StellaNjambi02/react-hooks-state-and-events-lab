7b1aba4f997b3c76f12c1159e7344822040103cc
StellaNjambi02 <njambindungistella@gmail.com>
2024-08-02 22:49:03 +0300

    states and events

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,27 @@
-import React from "react";
+
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+ 
+  const [isInCart, setIsInCart] = useState(false);
+
+  function toggleCart() {
+    setIsInCart((prev) => !prev);
+  }
+
+ 
+  const itemClass = isInCart ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={toggleCart}>
+        {isInCart ? "Remove from Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
 
 export default Item;
+
